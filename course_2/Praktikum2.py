2d4ff9dde06b3580900535caf3f0ae788f0df777
Kevin Gath <kevin@hgath.de>
2020-05-25 09:02:40 +0200

    Translation

--- course_2/Praktikum2.py
+++ course_2/Praktikum2.py
@@ -14,7 +14,7 @@ def getWitness(n):
     return cryptogen.randint(1, n - 1)
 
 
-# berechnet m hoch e mod n
+# calculates m ^ e mod n
 def modexp(m, e, n):
     if e == 0:
         return 1
@@ -22,8 +22,6 @@ def modexp(m, e, n):
         return m * modexp(m, e - 1, n) % n
     x = modexp(m, e // 2, n)
     p = x * x % n
-    # zusaetzliche Pruefung:
-    # x^2 = 1 mehr als die Loesungen 1 und -1
     if p == 1 and x != 1 and x != n - 1:
         raise
     return p
@@ -34,7 +32,7 @@ def isCompositeWitness(a, n):
         p = modexp(a, n - 1, n)
     except:
         return True
-    return p != 1  # p negieren, da kein zusammengesetzter Zeuge
+    return p != 1
 
 
 def isCompositeMillerRabin(n, k):
@@ -62,7 +60,6 @@ def monteCarloNextPrim(anz, n, it):
     for i in range(anz):
         randomNumber = cryptogen.randint(1, n)
         distance = 0
-        # print "Number = " + str(randomNumber)
 
         while isCompositeMillerRabin(randomNumber, it):
             randomNumber = randomNumber + 1
@@ -82,28 +79,27 @@ def countWitness(n):
     return count
 
 
-# Aufgabe 1
-print("Aufgabe 1:")
-print("Ist moeglicherweise Prim? " + str((not isCompositeMillerRabin(17, 5))))
-print("Ist moeglicherweise Prim? " + str((not isCompositeMillerRabin(32, 5))))
-print("Ist moeglicherweise Prim? " + str((not isCompositeMillerRabin((10 ** 100), 10))))
+print("Exercise 1:")
+print("Is possibly prime? " + str((not isCompositeMillerRabin(17, 5))))
+print("Is possibly prime? " + str((not isCompositeMillerRabin(32, 5))))
+print("Is possibly prime? " + str((not isCompositeMillerRabin((10 ** 100), 10))))
 
 print("")
 print("")
 
-# Aufgabe 2
-print("Aufgabe 2:")
-print("Naechste Primzahl : " + str(getNextPrim(17, 5)))
-print("Naechste Primzahl : " + str(getNextPrim(32, 5)))
-print("Naechste Primzahl : " + str(getNextPrim((10 ** 100), 10)))
+
+print("Exercise 2:")
+print("Next prime number : " + str(getNextPrim(17, 5)))
+print("Next prime number : " + str(getNextPrim(32, 5)))
+print("Next prime number : " + str(getNextPrim((10 ** 100), 10)))
 
 print("")
 print("")
 
-# Aufgabe 3
-print("Aufgabe 3:")
-print("Anzahl Zeugen: " + str(countWitness(9)))
-print("Anzahl Zeugen: " + str(countWitness(325)))
+
+print("Exercise 3:")
+print("Count witness: " + str(countWitness(9)))
+print("Count witness: " + str(countWitness(325)))
 
 print("")
 print("")
@@ -112,13 +108,13 @@ print("")
 def parallelSim(anz, rangeTo):
     fobj_out = open("generated_files/" + str(anz) + "_sims_nextPrim", "w")
     for i in drange(2, rangeTo, 0.5):
-        print(str(anz) + " Simulationen, i = " + str(i))
+        print(str(anz) + " Simulations, i = " + str(i))
         fobj_out.write(str((i * 2 + 1)) + "\t" + str(monteCarloNextPrim(anz, 100 ** i, 10)) + "\n")
 
     fobj_out.close()
 
 
-# Aufgabe 4
+# Exercise 4
 a = [50, 100, 200]
 processes = [mp.Process(target=parallelSim, args=(x, 50)) for x in a]
 
